f8c224473ae079b2c0ab87e47dd445c27785493d
Hemant <hrsmart2150@gmail.com>
2022-08-22 09:21:27 +0530

    Made some change

--- components/UserProfile.jsx
+++ components/UserProfile.jsx
@@ -19,7 +19,7 @@ const UserProfile = ({ user }) => {
           {/* profile and names  */}
           <div className='flex md:flex-col items-center mb-4 w-full lg:w-[350px] lg:self-end lg:pr-[20px] '>
             {/* profile image  */}
-            <div className='relative inline-block shrink-0 mx-4 w-[50px] md:w-[250px] lg:w-[320px] sm:w-[100px] md:mt-11 z-50  '>
+            <div className='relative inline-block shrink-0 mx-4 w-[50px] md:w-[250px] lg:w-[300px] sm:w-[100px] md:mt-11 z-50  '>
               {user.avatar_url ? <img className='w-full rounded-full border-[2px] border-[#30373C] ' src={user.avatar_url} alt="" />
                 :
                 <Image width={300} className='rounded-full' src={default_image} alt="default user" />}
@@ -36,16 +36,36 @@ const UserProfile = ({ user }) => {
 
           <div className='flex flex-col w-full lg:w-[350px] lg:self-end lg:pr-[20px] '>
             <div className='flex flex-col md:block' >
-              {/* Mobile Follow Button  */}
-              <div className='mb-3 mx-4 hidden md:block'>
-                <button type='button' onClick={() => setFollow(!follow)} className='w-full bg-[#20272D] py-1 font-semibold text-sm text-[#C9D0D8] rounded-md text-center border-[1px] border-solid border-[#363B42] hover:border-[#8A959E] hover:bg-[#454e55] '>{follow ? 'Follow' : 'Unfollow'}</button>
-              </div>
 
               {/* User Bio  */}
               {user.bio && <div className='w-full mb-3'>
                 {/* bio  */}
                 <p className='mb-3 mx-4 text-md text-[#C9D1D9] tracking-wide '>{user.bio}</p>
               </div>}
+
+              {/* Mobile Follow Button  */}
+              <div className='mb-3 mx-4 hidden md:block'>
+                <button type='button' onClick={() => setFollow(!follow)} className='w-full bg-[#20272D] py-1 font-semibold text-sm text-[#C9D0D8] rounded-md text-center border-[1px] border-solid border-[#363B42] hover:border-[#8A959E] hover:bg-[#454e55] '>{follow ? 'Follow' : 'Unfollow'}</button>
+              </div>
+
+              {/* followers and following after md  */}
+              <div className='hidden md:flex pl-5 mt-2'>
+                <div className='mb-3 flex'>
+                  {/* followers  */}
+                  <div className='hover:text-[#59A7FF] cursor-pointer flex items-center group transition-all duration-300 space-x-1'>
+                    <TbUsers size={17} className='text-[#8A949F] group-hover:text-[#59A7FF]' />
+                    <span className='text-[#C9D1D9] font-semibold group-hover:text-[#59A7FF] text-sm'>{user.followers > 1000 ? (user.followers / 1000).toFixed(1) + 'k' : user.followers}</span>
+                    <p className='cursor-pointer text-[#8A949F] group-hover:text-[#59A7FF] text-sm'>followers</p>
+                  </div>
+                  {/* dot  */}
+                  <p className='text-[#C9D1D9] font-semibold mx-1 self-center '>·</p>
+                  {/* following  */}
+                  <div className='flex hover:text-[#59A7FF] items-center cursor-pointer group transition-all duration-300 space-x-1'>
+                    <span className='text-[#C9D1D9] font-semibold group-hover:text-[#59A7FF] text-sm'>{user.following > 1000 ? (user.following / 1000).toFixed(1) + 'k' : user.following}</span>
+                    <p className='cursor-pointer text-[#8A949F] group-hover:text-[#59A7FF] text-sm'>following</p>
+                  </div>
+                </div>
+              </div>
 
               <div className='w-full pl-4 space-y-1 '>
                 {/* location  */}
@@ -75,7 +95,7 @@ const UserProfile = ({ user }) => {
 
 
               {/* followings and followers  */}
-              <div className='flex pl-4 mt-2'>
+              <div className='md:hidden flex pl-4 mt-2'>
                 <div className='mb-3 flex'>
                   {/* followers  */}
                   <div className='hover:text-[#59A7FF] cursor-pointer flex items-center group transition-all duration-300 space-x-1'>
